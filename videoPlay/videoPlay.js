94ba2256f6219aed57db3c0f9602fd2edbfe8cb4
codingWithRakesh <146563644+codingWithRakesh@users.noreply.github.com>
2023-11-01 11:21:15 +0530

    create video play page compete in HTML & CSS

--- videoPlay/videoPlay.js
+++ videoPlay/videoPlay.js
@@ -19,3 +19,35 @@ let hideNav = document.getElementById('hideNav')
 navId2.addEventListener('click',()=>{
 	hideNav.classList.toggle('displayNone')
 })
+
+// 3re nav
+
+let allId = document.getElementById('AllId');
+let rightBtn = document.getElementById('rightBtn');
+let leftBtn = document.getElementById('leftBtn');
+
+let navigateBox2 = document.getElementsByClassName('navigateBox')[0];
+
+navigateBox2.addEventListener('scroll', () => {
+    if (navigateBox2.scrollLeft == 0) {
+        leftBtn.classList.add('displayNone');
+        allId.classList.remove('displayNone');
+    }
+    else {
+        leftBtn.classList.remove('displayNone');
+        allId.classList.add('displayNone');
+    }
+})
+
+rightBtn.addEventListener('click', () => {
+    navigateBox2.scrollLeft += 90;
+    leftBtn.classList.remove('displayNone');
+})
+
+leftBtn.addEventListener('click', () => {
+    navigateBox2.scrollLeft -= 90;
+    if (navigateBox2.scrollLeft == 0) {
+        leftBtn.classList.add('displayNone');
+        allId.classList.remove('displayNone');
+    }
+})
